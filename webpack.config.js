774bc67bb88688de08c20d76437e7952e35cf4c9
Gustaf Gunér <gustaf@guner.se>
2018-08-14 11:07:26 +0200

    Support for fonts via file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,16 @@ module.exports = {
           } 
         }
       ]
+    },
+    {
+      test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: './assets/fonts/'
+        }
+      }
     }]
 	},
 	plugins: [
